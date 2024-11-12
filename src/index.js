31508ab87dd1e3804be91433c10c46d7c9941d95
monish1004 <22H51A05Q5@cmrcet.ac.in>
2024-11-12 22:17:48 +0530

    ping check

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const ServerConfig = require("./config/serverConfig");
 const connectDB = require("./config/dbConfig");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.text());
 
 const PORT = ServerConfig.PORT;
 
@@ -11,4 +15,3 @@ app.listen(PORT, async () => {
   await connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
-
